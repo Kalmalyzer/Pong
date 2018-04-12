f4905353545c227565ab15c385842f4a1058afdc
kalms@falldamagestudio.com <>
2018-04-12 13:14:24 +0200

    Movable paddle in place

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public string InputAxisName;
+    public float MovementSpeedScaleFactor;
+    public float PositionScale;
+
+    private float yPosition;
+    private MeshRenderer mesh;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        float delta = Input.GetAxis(InputAxisName) * MovementSpeedScaleFactor * Time.deltaTime;
+        yPosition = Mathf.Clamp(yPosition + delta, -1, 1);
+
+        transform.position = new Vector3(transform.position.x, yPosition * PositionScale, transform.position.z);
+    }
+}
