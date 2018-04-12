7a088c60bda5c1f8b34e78b94f265402431464e2
kalms@falldamagestudio.com <>
2018-04-12 15:16:27 +0200

    Player score logic moved to separate class

---
+++ Assets/Scripts/Logic/PlayerScoresLogic.cs
@@ -0,0 +1,27 @@
+﻿public class PlayerScoresLogic
+{
+    private readonly int pointsToWin;
+
+    private int[] scores = new int[2];
+
+    public PlayerScoresLogic(int pointsToWin)
+    {
+        this.pointsToWin = pointsToWin;
+    }
+
+    public int PlayerScore(int playerId)
+    {
+        return scores[playerId];
+    }
+
+    public void Reset()
+    {
+        for (int playerId = 0; playerId < 2; playerId++)
+            scores[playerId] = 0;
+    }
+
+    public void AddPoint(int playerId)
+    {
+        scores[playerId]++;
+    }
+}
