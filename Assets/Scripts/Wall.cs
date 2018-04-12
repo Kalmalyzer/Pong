f7f18b5e339fbb168b8c0ef059d42dd4a7b94a51
kalms@falldamagestudio.com <>
2018-04-12 13:43:53 +0200

    Bounce sfx when ball hits walls or paddle

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    private AudioSource bounceSfx;
+
+    void Start()
+    {
+        bounceSfx = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bounceSfx.Play();
+    }
+}
